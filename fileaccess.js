31e927835d98c82a0899730b3ce31dbfbe12fc99
Meang Akira Tanaka <meang.tnk@gmail.com>
2016-03-09 21:14:11 +0100

    Adding get git directories

--- fileaccess.js
+++ fileaccess.js
@@ -1,4 +1,38 @@
-function getFiles(root) {
+function getGitDirectories(root, blacklist) {
+    var result = [];
+
+    var currentDir = new java.io.File(root);
+    var files = currentDir.listFiles();
+
+    var hasGit = false;
+    for(var fileIndex in files) {
+        var file = files[fileIndex];
+        if(file.getName() === ".git") {
+            hasGit = true;
+            break;
+        }
+    }
+
+    if(hasGit) {
+        return result;
+    }
+
+    for(var fileIndex in files) {
+        var file = files[fileIndex];
+  
+        if(file.isDirectory()) {
+            var filename = file.getName();
+	    if(!(filename in blacklist) && filename.indexOf(".") != 0 ) {
+	        result.push(file);
+                result = result.concat(getGitDirectories(file.getCanonicalPath(), blacklist));
+    	    }
+	}
+    }
+    return result;
+};
+
+
+function getFilesRecursively(root) {
     var result = [];
 
     var currentDir = new java.io.File(root);
@@ -8,9 +42,28 @@ function getFiles(root) {
         result.push(file);
   
         if(file.isDirectory()) {
-	    result = result.concat(getFiles(file.toString()));
+	    result = result.concat(getFilesRecursively(file.toString()));
 	}
 
     }
     return result;
-}
+};
+
+function grep(filename, patternString) {
+    var imports = new JavaImporter(java.io);
+
+    with(imports) {
+        var bufferedReader = new BufferedReader(new FileReader(filename));
+
+        var pattern = new RegExp(patternString);
+
+        var lineCount = 0;
+        for(var line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+            lineCount++;
+            if(pattern.test(line)) {
+                print(lineCount + ": " + line);
+            }
+        }
+        bufferedReader.close();
+    }
+};
--- show.js
+++ show.js
@@ -2,32 +2,18 @@ load("fileaccess.js");
 
 // jjs -scripting <script> -- args
 
-//print($ARG[0]);
-
-//var result = getFiles(".");
-
-function grep(filename, patternString) {
-    var imports = new JavaImporter(java.io);
-
-    with(imports) {
-        var bufferedReader = new BufferedReader(new FileReader(filename));
-
-        var pattern = new RegExp(patternString);
-
-        var lineCount = 0;
-        for(var line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
-            lineCount++;
-            if(pattern.test(line)) {
-                print(lineCount + ": " + line);
-            }
-        }
-        bufferedReader.close();
-    }
-}
-
-grep($ARG[0], $ARG[1]);
+// var result = getFilesRecursively($ARG[0]);
 
 
+
+// grep($ARG[0], $ARG[1]);
+
+//var blacklist = {}; // or var map = {};
+//blacklist["RemoteSystemsTempFiles"] = 0;
+//var result = getGitDirectories($ARG[0], blacklist);
+//for(var file in result) {
+//    print(result[file]);
+//}
 
 
 //$EXEC("C:\\Program Files\\Git\\bin\\git.exe status")
